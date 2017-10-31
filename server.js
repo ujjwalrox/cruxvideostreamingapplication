b7258bcf3d6a15f158325ba948a39fed9ec5ccb2
Ujjwal Raizada <ujjwalraizada@gmail.com>
2017-10-31 23:18:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,17 @@ app.get('/newone.mp4', function(req, res){
 io.on('connection', function (socket){
   socket.on('CH01', function (from, msg) {
     console.log('Connected client to socket');
+    //Generating a Random Number
     randomNumberBetween0and19 = Math.floor(Math.random() * 10000000000000000);
+    
+    
     console.log('video link is '+randomNumberBetween0and19);
+    //Emiting that random link to the client
     socket.emit('videolink', randomNumberBetween0and19);
     app.get('/'+randomNumberBetween0and19, function(req, res){
       res.writeHead(200,{'content-type':'video/mp4'});
       var readstream = fs.createReadStream('sample.mp4');
+      //piping that video stream to the random generated link
       readstream.pipe(res);
     });
   });
